8f7d34ee30f1f298a439ce5e7d930f9952058a09
Jad-Bel <jadthegamer06@gmail.com>
2025-02-19 10:17:50 +0100

    FEAT: seeded categories table

--- app/Models/categories.php
+++ app/Models/categories.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'categorie_id';
+
+    protected $fillable = [
+        'categorie_name'
+    ];
 }
